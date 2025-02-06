cd76f8fea3c6a775f7a07105648520edb9d24cde
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-02-06 20:44:45 -0300

    refactor: remove classes

--- src/unioeste/geral/endereco/MyEnderecoGeralServicosMain.java
+++ src/unioeste/geral/endereco/MyEnderecoGeralServicosMain.java
@@ -2,7 +2,7 @@ package unioeste.geral.endereco;
 
 public class MyEnderecoGeralServicosMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Jar de MyEnderecoGeralServicos");
     }
 }
