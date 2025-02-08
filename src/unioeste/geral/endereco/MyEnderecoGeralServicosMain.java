c12bbdb6f4a79fd7d1e5bf75a95b9d89e46fac16
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-02-08 12:13:18 -0300

    feat: finish address services

--- src/unioeste/geral/endereco/MyEnderecoGeralServicosMain.java
+++
@@ -1,8 +0,0 @@
-package unioeste.geral.endereco;
-
-public class MyEnderecoGeralServicosMain {
-
-    public static void main(String[] args) {
-        System.out.println("Jar de MyEnderecoGeralServicos");
-    }
-}
---
+++ src/unioeste/geral/endereco/MyEnderecoServicosMain.java
@@ -0,0 +1,8 @@
+package unioeste.geral.endereco;
+
+public class MyEnderecoServicosMain {
+
+    public static void main(String[] args) {
+        System.out.println("Jar de MyEnderecoServicos");
+    }
+}
